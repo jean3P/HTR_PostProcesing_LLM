d8d33845bcd29716d0322ad64b22284ba42075d7
jean3P <jean.pereyraprincipe@students.unibe.ch>
2024-09-08 21:00:27 +0200

    Refactor training script with logging and dataset check
    Introduced logging setup and status reporting for the training process. Refactored dataset handling to check for the presence of HDF5 files before proceeding with training. This ensures better error handling and process tracking.

--- TrOCR_model/src/main.py
+++ TrOCR_model/src/main.py
@@ -4,16 +4,28 @@ import os
 from config.TrOCR_config import TrainingConfig
 from pipeline.pipeline_manager import main_training_pipeline
 from constans import outputs_hdf5, model_outputs, results_outputs
+from utils.logger import setup_logger, log_status
+
+# Set up the logger
+logger = setup_logger()
 
 
-# Example usage
 def run_training():
-    dataset_name = 'washington'
-    hdf5_file_path = os.path.join(outputs_hdf5, dataset_name, f'{dataset_name}_dataset.hdf5')
-    training_config = TrainingConfig()
+    datasets = ['washington']
 
-    # Call the main training pipeline to handle the full process
-    main_training_pipeline(dataset_name, hdf5_file_path, model_outputs, training_config, results_outputs)
+    # Iterate over each dataset
+    for dataset_name in datasets:
+        # Construct the HDF5 file path for the current dataset
+        hdf5_file_path = os.path.join(outputs_hdf5, dataset_name, f'{dataset_name}_dataset.hdf5')
+
+        # Check if the HDF5 file exists
+        if os.path.exists(hdf5_file_path):
+            # If it exists, set up the training configuration and run the pipeline
+            training_config = TrainingConfig()
+            main_training_pipeline(dataset_name, hdf5_file_path, model_outputs, training_config, results_outputs)
+        else:
+            # Log a warning or message if the dataset file is not found
+            log_status(f"Skipping dataset {dataset_name}: HDF5 file not found", logger)
 
 
 if __name__ == "__main__":
