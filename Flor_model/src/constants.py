09c5a251b87b19e52ac1825b9a216915559e6545
jean3P <jean.pereyraprincipe@students.unibe.ch>
2024-10-06 14:58:07 +0200

    saved data

--- Flor_model/src/constants.py
+++ Flor_model/src/constants.py
@@ -8,4 +8,5 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 outputs_hdf5 = os.path.join(current_dir, '../../Datasets/outputs')
 model_outputs = os.path.join(current_dir, '../models')
 results_outputs = os.path.join(current_dir, '../results')
+results_outputs_validation = os.path.join(results_outputs, 'validation')
 datasets = ['washington', 'bentham', 'iam']
